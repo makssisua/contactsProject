32f9eaa3da5a0ca6c18b608fbfeaeb41b8cffee8
Maksym Shymanskij <makskampod@gmail.com>
2022-05-02 16:30:11 +0300

    Restyling  navigation v2 active tab

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,13 +1,46 @@
 import React from "react";
+import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
 import "./Navigation.scss"
 
 export const Navigation = () => {
+  let url = useLocation();
+  
   return (
       <ul className="navigation">
-        <li className="navigation__button navigation__button"><Link to="/">home</Link></li>
-        <li className="navigation__button"><Link to="/contacts">contacts</Link></li>
-        <li className="navigation__button"><Link to="/about">about</Link></li>
+        <li className="navigation__button">
+          <Link 
+            className={url.pathname === '/' 
+              ? 'navigation__button--active-link' 
+              : 'navigation__button--link'
+            }
+            to="/"
+          >
+            home
+          </Link>
+        </li>
+        <li className="navigation__button">
+          <Link 
+            className={url.pathname === '/contacts' 
+            ? 'navigation__button--active-link' 
+            : 'navigation__button--link'
+          } 
+            to="/contacts"
+          >
+            contacts
+          </Link>
+        </li>
+        <li className="navigation__button">
+          <Link 
+            className={url.pathname === '/about' 
+            ? 'navigation__button--active-link' 
+            : 'navigation__button--link'
+          } 
+            to="/about"
+          >
+            about
+          </Link>
+        </li>
       </ul>
   )
 }
