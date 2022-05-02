f7db73a07b6dbb2b895435a63e8fde9cd7988df5
Maksym Shymanskij <makskampod@gmail.com>
2022-05-02 14:37:22 +0300

    Add routing

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.scss';
 import { Home } from '../components/Home/Home';
+import { About } from '../components/About/About'; 
+import { Contacts } from '../components/Contacts/Contacts'; 
 import  { Navigation } from "../components/Navigation/Navigation"
+import { Route, Routes } from 'react-router';
 
 function App() {
   return (
     <div className="App">
       <section className='App__content'>
-        <Home />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/contacts' element={<Contacts />}/>
+          <Route path='/about' element={<About />}/>
+          <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem", fontWeight: "bold" }}>
+                  <h1>Error 404</h1>
+                </main>
+              }
+            />
+        </Routes>
       </section>
       <section className='App__nav'>
         <Navigation />
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App/App';
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <App />
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
 );
