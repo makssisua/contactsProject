aea998cfa86ff48e64ee9d379869cb7027b45aec
Maksym Shymanskij <makskampod@gmail.com>
2022-05-02 16:35:28 +0300

    Create page404

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,7 +3,8 @@ import './App.scss';
 import { Home } from '../components/Home/Home';
 import { About } from '../components/About/About'; 
 import { Contacts } from '../components/Contacts/Contacts'; 
-import  { Navigation } from "../components/Navigation/Navigation"
+import { Navigation } from "../components/Navigation/Navigation"
+import { Page404 } from '../components/Error/404';
 import { Route, Routes } from 'react-router';
 
 function App() {
@@ -14,14 +15,7 @@ function App() {
           <Route path='/' element={<Home />}/>
           <Route path='/contacts' element={<Contacts />}/>
           <Route path='/about' element={<About />}/>
-          <Route
-              path="*"
-              element={
-                <main style={{ padding: "1rem", fontWeight: "bold" }}>
-                  <h1>Error 404</h1>
-                </main>
-              }
-            />
+          <Route path="*" element={<Page404 />}/>
         </Routes>
       </section>
       <section className='App__nav'>
