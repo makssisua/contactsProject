e6a79a27292291d8b78811cf71c38fd2f975fa5a
Maksym Shymanskij <makskampod@gmail.com>
2022-05-04 12:47:54 +0300

    Add contact search input

--- src/components/Contacts/ContactsList/ContactsList.tsx
+++ src/components/Contacts/ContactsList/ContactsList.tsx
@@ -1,27 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import "../ContactsList/ContactsList.scss" 
 import { Link } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../../hooks/redux";
 import { fetchUsers } from "../../../store/reducers/ActionCreators";
 import { ContactItem } from "../ContactItem/ContactItem";
+import { Contact } from "../../../interface/Contact";
 
-export const Contacts = () => {
+export const Contacts: React.FC = () => {
   const { contacts, isLoading, error } = useAppSelector(state => state.contactsReducer)
+  const [searchInput, setSearchInput] = useState('')
   const dispatch = useAppDispatch()
-  console.log('contacts', contacts)
 
   useEffect(() => {
     if (contacts.length === 0) {
       dispatch(fetchUsers())
     }
   }, [])
+
+  const filterContacts = (): Contact[] => {
+    let filteredContacts
+
+    return filteredContacts = contacts.filter(contact => (
+      contact.name.toLowerCase().includes(searchInput.toLocaleLowerCase())
+    ))
+  }
+  
+  const handleSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value)
+    filterContacts()
+  }
+
+  const filteredContacts = useMemo(filterContacts, [searchInput, contacts])
+
+  console.log(filteredContacts)
+  
   return (
     <>
       <h1>Contacts</h1>
       {isLoading && <h3>Loading...</h3>}
       {error && <h3>{error}</h3>}
+      <div className="input-group input-group-sm mb-3 search">
+        <input 
+          type="text"
+          value={searchInput}
+          placeholder="Search"
+          className="form-control"
+          onChange={(e) => handleSearchInput(e)}
+        />
+      </div>
       <ul className="list-group">
-        {contacts.map(contact => 
+        {filteredContacts.map(contact => 
           <li 
             key={contact.id} 
             className="list-group-item item"
